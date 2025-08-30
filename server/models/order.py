54a1d9fccffed2d55d89302e8f51cca51f7031db
Ernest Kamau <1.kamauernest@gmail.com>
2025-08-31 01:16:24 +0300

    adds order model

---
+++ server/models/order.py
@@ -0,0 +1,14 @@
+from extensions import db
+from datetime import datetime, timezone
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    description = db.Column(db.Text)
+    status = db.Column(db.Enum("pending", "paid", "cancelled"), default="pending")
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    
+    items = db.relationship("OrderItem", back_populates="order", cascade='all, delete-orphan')
+    
