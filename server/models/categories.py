f66b855654a6fccf0860d720b7367aa4f10f3eec
Ernest Kamau <1.kamauernest@gmail.com>
2025-08-31 00:46:15 +0300

    adds category model

---
+++ server/models/categories.py
@@ -0,0 +1,10 @@
+from extensions import db
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    
+    products = db.relationship('Product', back_populates='category', cascade='all, delete-orphan')
+    
