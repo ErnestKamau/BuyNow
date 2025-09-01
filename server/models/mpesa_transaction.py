3b0421e5fa410eb450393325d410ec9364c33032
Ernest Kamau <1.kamauernest@gmail.com>
2025-09-01 11:26:33 +0300

    adds mpesa-transactions model

---
+++ server/models/mpesa_transaction.py
@@ -0,0 +1,24 @@
+from extensions import db
+from datetime import datetime, timezone, timedelta
+
+class MpesaTransaction(db.Model):
+    __tablename__ = 'mpesa_transactions'
+    
+    transaction_id = db.Column(db.String(100), primary_key=True)  # M-Pesa transaction ID
+    checkout_request_id = db.Column(db.String(100), unique=True)
+    merchant_request_id = db.Column(db.String(100))
+    amount = db.Column(db.Numeric(10, 2), nullable=False)
+    phone_number = db.Column(db.String(15), nullable=False)
+    account_reference = db.Column(db.String(100))  # Could be order_id
+    transaction_desc = db.Column(db.String(200))
+    status = db.Column(db.Enum("pending", "success", "failed", "cancelled", name="mpesa_status"), default="pending", nullable=False)
+    result_code = db.Column(db.Integer)
+    result_desc = db.Column(db.String(200))
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    completed_at = db.Column(db.DateTime)
+    
+    
+    sale = db.relationship("Sale", back_populates="transaction")
+    
+    def __repr__(self):
+        return f'<MpesaTransaction {self.transaction_id}>'
